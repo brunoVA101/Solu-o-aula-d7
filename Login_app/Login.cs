b528a9b09234dad09bc90c58fbff271fdd9487b5
brunoVA101 <brunovalcantara@hotmail.com>
2022-10-10 19:14:12 -0300

    fist commit

---
+++ Login_app/Login.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Login_app
+{
+    public class Login
+    {
+        public string user { get; set; }
+        public string password { get; set; }
+
+        public Login(string user, string password)
+        {
+            this.user = user;
+            this.password = password;
+        }
+
+        private readonly static string stringConnection = "Data source=BRUNOPC\\SQLEXPRESS; initial catalog=Accounts; user id=sa; pwd=bruno123;";
+        public bool validate()
+        {
+            using (SqlConnection con = new SqlConnection(stringConnection))
+            {
+                string querySelectAll = "SELECT ID, Name, Email, Password FROM Users";
+
+                con.Open();
+
+                SqlDataReader rdr;
+
+                using (SqlCommand cmd = new(querySelectAll, con))
+                {
+                    rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+
+                        if (rdr["Email"].ToString() == user && rdr["Password"].ToString() == password)
+                        {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ Login_app/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Login_app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Acessar_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("foi1");
+            var Login_attempt = new Login(Usuario.Text, Senha.Text);
+            Console.WriteLine("foi2");
+            try
+            {
+                if (Login_attempt.validate())
+                {
+                    Main_Grid.Children.Add(new Message_success());
+                }
+                else
+                {
+                    Main_Grid.Children.Add(new Message_failure());
+                }
+            }
+            catch (Exception err)
+            {
+                Usuario.Text = err.Message;
+            }
+        }
+    }
+}
